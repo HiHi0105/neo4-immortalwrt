d912e602ff00edd0c6ed13ca8f804b1b2451b16f
HiHi0105 <hihi0105@gmail.com>
2026-05-08 11:04:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-
-# Lucky
-git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
-
-# ImmortalWrt feeds
-sed -i 's|https://git.openwrt.org/feed/|https://github.com/immortalwrt/packages.git|g' feeds.conf.default
+# 不做任何 feeds 修改
+echo "Skip custom feeds"
